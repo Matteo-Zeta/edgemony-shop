139a35993fc1a5d260316f48817925b5b19d6bcf
Matteo Zappalà <matteozappala@hotmail.it>
2021-03-02 19:33:10 +0100

    Fix layout

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,10 @@
 import './Body.css';
+import Hero from './Hero'
 
 function Body(props) {
   const {title, description, cover} = props;
   return <main className="Body">
-    <div className="container">
-      <img src={cover} alt="the hero cover"/>
-      <div className="hero-title">
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-      </div>
-    </div>
+      <Hero title={title} description={description} cover={cover}></Hero>
   </main>
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import './Header.css';
 function Header(props) {
   return <header className="Header">
     <img id="logo" src={props.imageSrc} alt='logo' />
-    <button>Get started</button>
     <div>
-      <span>Docenti</span>
-      <span>Digital marketing</span>
-      <span>Sviluppo</span>
+      <span>Donna</span>
+      <span>Uomo</span>
+      <span>Bambini</span>
+    <strong>ACCEDI</strong>
     </div>
   </header>
 }
