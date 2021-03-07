1e571425636531d38187b83526763647b98da30e
Matteo Zappalà <matteozappala@hotmail.it>
2021-03-07 02:19:12 +0100

    Fixed tablet layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,14 @@ import './Hero.css';
 function Hero(props) {
   const { title, description, cover } = props;
   return <div style={{ backgroundImage: `url(${cover})` }} className="hero">
-    {/* <img src={cover} alt="the hero cover" /> */}
     <div className="hero-title">
       <strong>BENVENUTO SU EDGEMONY SHOP</strong>
       <h1>{title}</h1>
       <h2>{description}</h2>
       <p>Nel nostro esclusivo shopping club ti aspettano imperdibili offerte giornaliere
-        sui migliori brand di abbigliamento, scarpe, borse ed accessori per tutta la famiglia. 
-        Approfitta delle nostre vendite private con sconti fino al 75%!</p>
-        <button type='button'>ISCRIVITI ORA</button>
+      sui migliori brand di abbigliamento, scarpe, borse ed accessori per tutta la famiglia.
+      Approfitta delle nostre vendite private con sconti fino al 75%!</p>
+      <button type='button'>ISCRIVITI ORA</button>
     </div>
   </div>
 }
